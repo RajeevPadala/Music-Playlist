e3b2d8f0f4f55d89d20057cd7f0de0db715ff4e3
RajeevPadala <razeev198@gmail.com>
2023-04-16 12:59:16 +0000

    Music Playlist

---
+++ src/components/MusicItem/index.js
@@ -0,0 +1,42 @@
+import {AiOutlineDelete} from 'react-icons/ai'
+
+import {
+  MusicListItem,
+  TrackInfoContainer,
+  MusicTrackImage,
+  NameGenreContainer,
+  Name,
+  Genre,
+  DurationDeleteContainer,
+  Duration,
+  DeleteButton,
+} from './styledComponents'
+
+const MusicItem = props => {
+  const {MusicItemDetails, onClickDeleteTrack} = props
+  const {id, imageUrl, name, genre, duration} = MusicItemDetails
+
+  const onDeleteTrack = () => {
+    onClickDeleteTrack(id)
+  }
+
+  return (
+    <MusicListItem>
+      <TrackInfoContainer>
+        <MusicTrackImage src={imageUrl} alt="track" />
+        <NameGenreContainer>
+          <Name>{name}</Name>
+          <Genre>{genre}</Genre>
+        </NameGenreContainer>
+      </TrackInfoContainer>
+      <DurationDeleteContainer>
+        <Duration>{duration}</Duration>
+        <DeleteButton type="button" testid="delete" onClick={onDeleteTrack}>
+          <AiOutlineDelete size={25} />
+        </DeleteButton>
+      </DurationDeleteContainer>
+    </MusicListItem>
+  )
+}
+
+export default MusicItem
